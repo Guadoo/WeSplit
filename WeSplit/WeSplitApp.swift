b0c689c1f83499937289c5738e7f26ecb03437d2
Liu Jingkai <rock_jk@msn.com>
2021-05-06 17:27:55 +0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Guadoo on 2021/5/6.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
